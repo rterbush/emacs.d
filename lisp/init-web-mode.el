3cc5ebbf3c95f9b019ead06edeb0c4df661235fa
Randy Terbush <randy@tribalknowledgegroup.com>
2015-02-25 16:21:25 -0700

    Cleanup merge mess.

--- lisp/init-web-mode.el
+++ lisp/init-web-mode.el
@@ -9,14 +9,11 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-<<<<<<< HEAD
 (add-to-list 'auto-mode-alist '("\\.hbs$\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.handlebars$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (setq web-mode-enable-current-element-highlight t)
 (setq web-mode-enable-auto-pairing t)
 ;;(set-face-attribute 'web-mode-html-tag-face nil)
-=======
->>>>>>> Fix up loading on Linux
 
 (provide 'init-web-mode)
