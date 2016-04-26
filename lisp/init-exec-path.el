5c76101604ede821bb29d88cf9df7f83d7218719
Randy Terbush <randy@tribalknowledgegroup.com>
2016-04-26 17:10:12 -0600

    Provide workaround for Emacs bug that causes exec-path-from-shell-initialize to hang on FreeBSD.

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -4,7 +4,7 @@
   (dolist (var '("PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
-
+(setq exec-path-from-shell-arguments '("-c"))
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
