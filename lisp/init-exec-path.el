2d1b6e0256f9bf491c725c885f9f9462d5be7ca1
Randy Terbush <randy@lifeguardmobile.com>
2016-05-02 17:13:39 -0600

    Fix up some portability issues with exec-path-from-shell

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,13 +1,16 @@
+;;; package --- Summary:
+;;; Commentary:
 (require-package 'exec-path-from-shell)
 
+;;; Code:
 (after-load 'exec-path-from-shell
   (dolist (var '("PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
 
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+(when (memq window-system '(mac))
+      (exec-path-from-shell-initialize))
 
-(exec-path-from-shell-initialize)
 
 (provide 'init-exec-path)
+;;; init-exec-path.el ends here
--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -2,5 +2,6 @@
 (setq recentf-max-saved-items 1000
       recentf-exclude '("/tmp/" "/ssh:"))
 
+(setq recentf-keep '(file-remote-p file-readable-p))
 
 (provide 'init-recentf)
