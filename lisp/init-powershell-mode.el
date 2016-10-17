88cf8ddfe93e6195cdd44b2e182d065484e0a331
Randy Terbush <randy@lifeguardmobile.com>
2016-10-17 09:07:22 -0600

    Updates to local powershell setup

--- lisp/init-powershell-mode.el
+++ lisp/init-powershell-mode.el
@@ -5,5 +5,8 @@
 (autoload 'powershell-mode "powershell-mode" "Mode PowerShell" t)
 (add-to-list 'auto-mode-alist '("\\.ps[12]?$" . powershell-mode))
 
+(add-hook 'powershell-mode-hook
+          (setq powershell-indent 4))
+
 (provide 'init-powershell-mode)
 ;;; init-powershell-mode.el ends here
